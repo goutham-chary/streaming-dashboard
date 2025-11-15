2358c56867bc8d8993778a7176369404de68ed67
goutham-chary <gouthamchary00@gmail.com>
2025-11-15 11:10:44 +0530

    second commit

--- app/api/tmdb/route.ts
+++ app/api/tmdb/route.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function GET(request) {
+export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const query = searchParams.get("query");
